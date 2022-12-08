4d3b0c3fed4f439d081a5bb143c2c5ca610fcab7
Meonardo <sisxme@gmail.com>
2022-12-08 09:49:36 +0800

    restore record/stream video fps to 30.

--- UI/window-basic-main.cpp
+++ UI/window-basic-main.cpp
@@ -1510,9 +1510,9 @@ bool OBSBasic::InitBasicConfigDefaults()
 	}
 
 	config_set_default_uint(basicConfig, "Video", "FPSType", 0);
-	config_set_default_string(basicConfig, "Video", "FPSCommon", "25 PAL");
-	config_set_default_uint(basicConfig, "Video", "FPSInt", 25);
-	config_set_default_uint(basicConfig, "Video", "FPSNum", 25);
+	config_set_default_string(basicConfig, "Video", "FPSCommon", "30");
+	config_set_default_uint(basicConfig, "Video", "FPSInt", 30);
+	config_set_default_uint(basicConfig, "Video", "FPSNum", 30);
 	config_set_default_uint(basicConfig, "Video", "FPSDen", 1);
 	config_set_default_string(basicConfig, "Video", "ScaleType", "bicubic");
 	config_set_default_string(basicConfig, "Video", "ColorFormat", "NV12");
