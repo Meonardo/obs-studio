f130ca3a71bbd0b9c6279fd4fe658b1dac75409f
Meonardo <sisxme@gmail.com>
2023-12-22 13:48:48 +0800

    Restore x264 module.

--- cmake/common/buildspec_common.cmake
+++ cmake/common/buildspec_common.cmake
@@ -170,7 +170,7 @@ function(_check_dependencies)
       list(APPEND CMAKE_PREFIX_PATH "${dependencies_dir}/${destination}")
     elseif(dependency STREQUAL qt6)
       message(WARN "Qt6 deps ${CMAKE_PREFIX_PATH}")
-      # list(APPEND CMAKE_PREFIX_PATH "${dependencies_dir}/${destination}")
+      list(APPEND CMAKE_PREFIX_PATH "${dependencies_dir}/${destination}")
     elseif(dependency STREQUAL cef)
       set(CEF_ROOT_DIR
           "${dependencies_dir}/${destination}"
